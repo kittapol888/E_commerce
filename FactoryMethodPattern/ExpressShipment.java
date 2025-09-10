1e541bbe93fdf4bc5c06c9b1462aaf62a5410ffd
kittapol888 <kittapol.sa@ku.th>
2025-09-11 04:36:45 +0700

    Create ExpressShipment.java

---
+++ FactoryMethodPattern/ExpressShipment.java
@@ -0,0 +1,17 @@
+package FactoryMethodPattern;
+/**
+ * Concrete Product: การจัดส่งแบบด่วน
+ */
+public class ExpressShipment implements Shipment {
+
+    @Override
+    public String getInfo() {
+        return "Express Delivery";
+    }
+
+    @Override
+    public double getCost() {
+        return 150.0;
+    }
+    
+}
