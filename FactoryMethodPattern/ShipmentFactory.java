cf47a40d3528c96c0e6581af8a9ed18757fcc71b
kittapol888 <kittapol.sa@ku.th>
2025-09-11 04:36:06 +0700

    Create ShipmentFactory.java

---
+++ FactoryMethodPattern/ShipmentFactory.java
@@ -0,0 +1,15 @@
+package FactoryMethodPattern;
+/**
+ * Factory สำหรับสร้างอ็อบเจกค์ Shipment
+ */
+public class ShipmentFactory {
+    public Shipment createShipment(String type){
+        if ("STANDARD".equalsIgnoreCase(type)) {
+            return new StandardShipment();
+        } else if ("EXPRESS".equalsIgnoreCase(type)) {
+            return new ExpressShipment();
+        }
+        throw new IllegalArgumentException("Unknown shipment type: " + type);
+    }
+    
+}
